4e166c0e1caf1d0de5b235683666b50a63eb02b7
xmikula <xmikula@stuba.sk>
2023-12-07 20:41:16 +0100

    Adjust curl test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L -F "name=inu.jpg" -F "file=@inu.jpg" localhost/detection --output detection.jpg
-curl -L -F "name=inu.jpg" -F "file=@inu.jpg" localhost/segmentation --output segmentation.jpg
+curl -L -F "name=inu.jpg" -F "file=@inu.jpg" localhost:6001/detection --output detection.jpg
+curl -L -F "name=inu.jpg" -F "file=@inu.jpg" localhost:6001/segmentation --output segmentation.jpg
