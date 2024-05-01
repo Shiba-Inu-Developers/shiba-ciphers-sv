186b75c7eace6cfdbf4bab4b01c532a8c9995a71
xmikula <62661841+xmikula@users.noreply.github.com>
2024-05-02 00:15:00 +0200

    Implement `classify` endpoint

--- app/main.py
+++ app/main.py
@@ -24,6 +24,15 @@ def segmentation(file: bytes = File(...), name: str = Form(...)) -> FileResponse
     return use_model(segmentation_model, file, name)
 
 
+@app.post("/classify")
+def classify(image: bytes = File(...), hash: str = Form(...)):
+    bytes = io.BytesIO()
+    image = Image.open(io.BytesIO(image))
+    image.save(bytes, format=image.format)
+    r.set(hash, bytes.getvalue())
+    return {"type": "key", "confidence": {"key": 0.8, "cyphertext": 0.1, "other": 0.1}}
+
+
 def use_model(model, file, name) -> FileResponse:
     id = uuid.uuid4().hex + os.path.splitext(name)[1]
     image = Image.open(io.BytesIO(file))
